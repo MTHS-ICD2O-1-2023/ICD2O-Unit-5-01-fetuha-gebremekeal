f8ddc666dbac0d96f04632f7858281c55fc1b22e
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-04-29 17:57:45 +0000

    name

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
 // Created by: Fetuha
-// Created on: Apr 2024
+// Created on: April 2024
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
+// variable
+const randomNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
 
-/**
- * This function checks if the user has selected the random number from above
- */
+function myButtonClicked() {
+  const userNumberGuess = parseInt(document.getElementById("user-number-guess").value)
 
-function checkNumber() {
-  // input
-  const userNumber = parseInt(document.getElementById("integer").value)
-
-  //process
-  if (userNumber == randomNumber) {
-    // output
-    document.getElementById("result").innerHTML =
-    "You got it!"
+  if (userNumberGuess == randomNumber) {
+    //output
+    document.getElementById("number-guess-output").innerHTML = "You guessed correctly!"
   }
 
-  if (userNumber != randomNumber) {
-    // output
-    document.getElementById("result").innerHTML = 
-    "You didn't get it!"
+  if (userNumberGuess != randomNumber) {
+    //output
+    document.getElementById("number-guess-output").innerHTML = "You guessed incorrectly!"
+  }
+
+  if (userNumberGuess > 6) {
+    //output
+    document.getElementById("number-guess-output").innerHTML = "Please input a number from 1 to 6!"
+  }
+
+  if (userNumberGuess < 1) {
+    //output
+    document.getElementById("number-guess-output").innerHTML = "Please input a number from 1 to 6!"
   }
 }
